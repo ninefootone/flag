eea97aeb3d962ecd650c573613cd6515c79c7651
Jon Bradley <jon@MacBookPro.lan>
2025-09-25 06:53:17 +0100

    20250924 v3.0.5.1 Styling

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '3.0.5;
+    const appVersion = '1.7';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
