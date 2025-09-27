fa29fca3c49cc110d176df853ce876ec70db3afd
Jon Bradley <jon@MacBookPro.lan>
2025-09-27 22:34:57 +0100

    20250924 v3.0.80 Share Buttons Hide v3

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '3.0.79';
+    const appVersion = '3.0.80';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
