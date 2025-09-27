a85cf9e0ee44112be394ec65745624a6d058a84c
Jon Bradley <jon@MacBookPro.lan>
2025-09-27 22:59:26 +0100

    20250924 v3.0.85 Share Buttons Game ID Styling

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '3.0.84';
+    const appVersion = '3.0.85';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
