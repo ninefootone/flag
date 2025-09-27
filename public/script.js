b020e098180f54f6a2f778b5cc07195bcbb71f50
Jon Bradley <jon@MacBookPro.lan>
2025-09-27 21:42:27 +0100

    20250924 v3.0.75 Safari Fix 6

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '3.0.74';
+    const appVersion = '3.0.75';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
