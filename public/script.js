3d275e8463ea1342d06aa2848ba179d7f94ccf8c
Jon Bradley <jon@MacBookPro.lan>
2025-09-27 21:37:50 +0100

    20250924 v3.0.74 Safari Fix 4

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '3.0.73';
+    const appVersion = '3.0.74';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
