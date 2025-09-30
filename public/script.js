c6476bdb0e8fb83c5dfdba0b2441e099bcb82cda
Jon Bradley <jon@Jons-MacBook-Pro-277.local>
2025-09-30 13:27:19 +0100

    20250930 v3.0.955 Font Weights

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '3.0.954';
+    const appVersion = '3.0.955';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
