3fee5a7e0fd337c3b5e159d3d3981c90aed4acfc
Jon Bradley <jon@Jons-MacBook-Pro-277.local>
2025-09-30 12:57:26 +0100

    20250930 v3.0.952 Inter

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '3.0.951';
+    const appVersion = '3.0.952';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
