5136e1fa495334da2cf7b53e7986c46180998c33
Jon Bradley <jon@MacBookPro.lan>
2025-10-03 21:30:09 +0100

    20251003 v0.0.19 More Horizontal Scroll Fixes

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.0.18';
+    const appVersion = '0.0.19';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
