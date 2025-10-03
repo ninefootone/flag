3fea606b3a58c261d6b8db5327d7333860ecdb27
Jon Bradley <jon@MacBookPro.lan>
2025-10-03 21:06:57 +0100

    20251003 v0.0.17 Penalty Styling Scrollable Table

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.0.16';
+    const appVersion = '0.0.17';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
