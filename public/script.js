9c4fe7f4cd008456ed661341f1dbf1c5a57d26b8
Jon Bradley <jon@MacBookPro.lan>
2025-10-05 21:52:45 +0100

    20251005 v0.0.37 Welcome Screen Styling v2

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.0.36';
+    const appVersion = '0.0.37';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
