453837c9cb23a62ba98cbe6bd05b841e8d581af5
Jon Bradley <jon@MacBookPro.lan>
2025-10-06 08:10:09 +0100

    20251005 v0.0.44 Metropolis Font and Form CSS

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.0.43';
+    const appVersion = '0.0.44';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
