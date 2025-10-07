729b02454ece311ad7d451c21a820faa972cc940
ninefootone <ninefootone@ninefooesiMac10.lan>
2025-10-07 15:39:17 +0100

    20251007 v0.0.65 Add info modal

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.0.64';
+    const appVersion = '0.0.65';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
