671d15b48fa4a339993b1a8375c230bcfb4e1ae3
ninefootone <ninefootone@ninefooesiMac10.lan>
2025-10-07 10:18:31 +0100

    20251007 v0.0.53 Hiding Elements

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.0.52a';
+    const appVersion = '0.0.53';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
