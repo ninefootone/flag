d5f835cc4313f3e6096095f082ad20da67638204
ninefootone <ninefootone@ninefooesiMac10.lan>
2025-10-08 12:58:51 +0100

    20251007 v0.0.89 Remove Audio iOS Safari Fixes

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.0.88';
+    const appVersion = '0.0.89';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
