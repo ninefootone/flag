81c63a8e83c5b9585dc8c3daa3dd9e409993eece
ninefootone <ninefootone@ninefooesiMac10.lan>
2025-10-09 14:13:49 +0100

    20251009 v0.1.08 New Logo

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.1.07';
+    const appVersion = '0.1.08';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
