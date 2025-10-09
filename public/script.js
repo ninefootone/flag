9b072246ccdb105bcd0a66370a1dac488ea534f8
ninefootone <ninefootone@ninefooesiMac10.lan>
2025-10-09 09:09:38 +0100

    20251008 v0.1.06 Greycliffe

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.1.05';
+    const appVersion = '0.1.06';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
