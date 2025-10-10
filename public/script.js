1cfb5432542bb557a1dbe0baf2cdd42fc3df2ce7
Jon Bradley <jon@MacBookPro.lan>
2025-10-10 21:03:56 +0100

    20251010 v0.1.31 Colors

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.1.30';
+    const appVersion = '0.1.31';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
