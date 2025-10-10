2bc65f5551b322b01ef9cf1c967a2729f26246e8
ninefootone <ninefootone@ninefooesiMac10.lan>
2025-10-10 11:18:39 +0100

    20251010 v0.1.21 PWA

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.1.20';
+    const appVersion = '0.1.21';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
