daebb93ca59dab040370c8902bd4aa2bebd01dd3
Jon Bradley <jon@MacBookPro.lan>
2025-10-11 07:44:46 +0100

    20251010 v0.1.36 Footer Links Floating

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.1.35';
+    const appVersion = '0.1.36';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
