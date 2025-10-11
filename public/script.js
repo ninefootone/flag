6220cc319d012b376a97926416d1ba9ec563dfba
Jon Bradley <jon@MacBookPro.lan>
2025-10-11 16:21:14 +0100

    20251011 v0.1.45 Dark Mode

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.1.44';
+    const appVersion = '0.1.45';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
