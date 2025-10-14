e5b3648dd417c544729676caa737e81831336b31
Jon Bradley <jon@MacBookPro.lan>
2025-10-14 21:00:47 +0100

    20251014 v0.1.57 Content Amends

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.1.56';
+    const appVersion = '0.1.57';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
