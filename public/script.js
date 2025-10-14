75e086308afc2a6ac88549c602ce5c5ae3a56c2f
Jon Bradley <jon@MacBookPro.lan>
2025-10-14 22:19:03 +0100

    20251014 v0.1.62 Fix MySite

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.1.61';
+    const appVersion = '0.1.62';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
