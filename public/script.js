82a719ad42c7c0c32cb4c411b3c85caed2429c03
Jon Bradley <jon@MacBookPro.lan>
2025-10-23 13:24:25 +0100

    20251023 v0.2.27 Coin Toss

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.2.26';
+    const appVersion = '0.2.27';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
