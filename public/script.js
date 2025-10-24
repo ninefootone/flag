92d9438e9996d1b3e3774999c3ff591405716f24
Jon Bradley <jon@ninefootone.co.uk>
2025-10-24 15:05:47 +0100

    20251023 v0.2.32 Coin Toss

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.2.31';
+    const appVersion = '0.2.32';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'referee-app-cache-v0.2.31';
+const CACHE_NAME = 'referee-app-cache-v0.2.32';
 const urlsToCache = [
   '/',
   '/index.html',
