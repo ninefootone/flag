7ac5609318a83f52f32018c25786d2552fb48253
Jon Bradley <jon@MacBookPro.lan>
2025-10-27 21:25:50 +0000

    20251027 v0.2.59 Opera Bug

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.2.58';
+    const appVersion = '0.2.59';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
