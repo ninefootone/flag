4e75d548ff06dff35e44300a7b7ff757d9727df4
Jon Bradley <jon@MacBookPro.lan>
2025-10-27 22:08:57 +0000

    20251027 v0.2.61 Opera Bug

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.2.60';
+    const appVersion = '0.2.61';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
@@ -481,7 +481,7 @@ fetchAndLoadTeamNames();
                 
                 setTimeout(() => {
                     connectWebSocket(gameIdFromUrl);
-                    updateUI();
+                    // updateUI();
                 }, 150); // 150ms delay gives Safari time to stabilize
             } else {
                 console.error("Maximum reconnect attempts reached. Please refresh.");
