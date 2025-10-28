350e33119821030e0e6c13dbeeb282307c2b34d6
Jon Bradley <jon@MacBookPro.lan>
2025-10-28 22:07:09 +0000

    20251028 v0.2.71 Upload Logos

--- public/script.js
+++ public/script.js
@@ -27,7 +27,7 @@ const clampInput = (inputElement, min, max) => {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    const appVersion = '0.2.70';
+    const appVersion = '0.2.71';
     console.log(`Referee App - Version: ${appVersion}`);
     const versionDisplay = document.querySelector('.version');
     if (versionDisplay) {
