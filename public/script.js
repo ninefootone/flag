3a033568d0924fbf87970e2e370efe665dcbf5d8
Jon Bradley <jon@MacBookPro.lan>
2025-11-01 18:06:55 +0000

    20251101 v0.3.11 Compact View

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const appVersion = '0.3.10';
+const appVersion = '0.3.11';
 console.log(`Referee App - Version: ${appVersion}`);
 
 /**
--- public/teams.js
+++ public/teams.js
@@ -1,4 +1,4 @@
-const teamsDataVersion = '0.3.10'; 
+const teamsDataVersion = '0.3.11'; 
 
 // Use 'window' to explicitly declare TEAM_LIST as a global variable.
 window.TEAM_LIST = []; 
