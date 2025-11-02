7746f8116a4c72d3256dadba2754391b8af7ce25
Jon Bradley <jon@MacBookPro.lan>
2025-11-02 20:54:32 +0000

    20251102 v0.3.26 Sticky Header

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const appVersion = '0.3.25';
+const appVersion = '0.3.26';
 console.log(`Referee App - Version: ${appVersion}`);
 
 /**
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'referee-app-cache-v0.3.25';
+const CACHE_NAME = 'referee-app-cache-v0.3.26';
 const urlsToCache = [
   '/',
   '/index.html',
