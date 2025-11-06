f5a50158382070a60cdd09164ac1ae5aa8b6f7d3
Jon Bradley <jon@MacBookPro.lan>
2025-11-06 21:40:04 +0000

    20251106 v0.3.43 Whistle Stats Again

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'referee-app-cache-v0.3.42';
+const CACHE_NAME = 'referee-app-cache-v0.3.43';
 const urlsToCache = [
   '/',
   '/index.html',
--- public/teams.js
+++ public/teams.js
@@ -1,4 +1,4 @@
-const teamsDataVersion = '0.3.42'; 
+const teamsDataVersion = '0.3.43'; 
 
 // Use 'window' to explicitly declare TEAM_LIST as a global variable.
 window.TEAM_LIST = []; 
