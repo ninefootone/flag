9cd19bb1cb4b4447fb3279bc8d83a2b08048ecc8
Jon Bradley <jon@MacBookPro.lan>
2025-11-10 20:33:47 +0000

    20251108 v0.3.72 Aggregated Stats Styling

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const appVersion = '0.3.71';
+const appVersion = '0.3.72';
 console.log(`Referee App - Version: ${appVersion}`);
 
 /**
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'referee-app-cache-v0.3.71';
+const CACHE_NAME = 'referee-app-cache-v0.3.72';
 const urlsToCache = [
   '/',
   '/index.html',
