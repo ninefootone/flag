f09b4a2c403b896560bc8fc662dcbd1f2f6690b8
Jon Bradley <ninefootone@Jons-Mini-M4.lan>
2026-03-04 14:10:18 +0000

    20260304 v0.3.85 Reconnect Fixes

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const appVersion = '0.3.84';
+const appVersion = '0.3.85';
 console.log(`Referee App - Version: ${appVersion}`);
 
 /**
--- public/teams.js
+++ public/teams.js
@@ -1,4 +1,4 @@
-const teamsDataVersion = '0.3.84'; 
+const teamsDataVersion = '0.3.85'; 
 
 // Use 'window' to explicitly declare TEAM_LIST as a global variable.
 window.TEAM_LIST = []; 
