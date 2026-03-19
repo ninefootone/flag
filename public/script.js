6bd0bbcdc2fc28a94a45976459cd997ee862795a
Jon Bradley <ninefootone@Jons-Mini-M4.lan>
2026-03-19 14:12:11 +0000

    0.4.03 Style Whistle Game Code

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const appVersion = '0.4.02';
+const appVersion = '0.4.03';
 console.log(`Referee App - Version: ${appVersion}`);
 
 /**
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'referee-app-cache-v0.4.02';
+const CACHE_NAME = 'referee-app-cache-v0.4.03';
 const urlsToCache = [
   '/',
   '/index.html',
