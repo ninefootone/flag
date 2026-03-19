8b212f082400a3caed4fb963d7aa0c9058db2e74
Jon Bradley <ninefootone@Jons-Mini-M4.lan>
2026-03-19 15:14:50 +0000

    0.4.09 Gridiron theme as dark mode only

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const appVersion = '0.4.08';
+const appVersion = '0.4.09';
 console.log(`Referee App - Version: ${appVersion}`);
 
 /**
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'referee-app-cache-v0.4.08';
+const CACHE_NAME = 'referee-app-cache-v0.4.09';
 const urlsToCache = [
   '/',
   '/index.html',
