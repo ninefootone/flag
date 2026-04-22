badf66a7839cf4d312ab8829c4342d21875c740a
Jon Bradley <jon@ninefootone.co.uk>
2026-04-22 17:39:27 +0100

    Bump to v0.4.57

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const appVersion = '0.4.56';
+const appVersion = '0.4.57';
 console.log(`Referee App - Version: ${appVersion}`);
 
 /**
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'referee-app-cache-v0.4.56';
+const CACHE_NAME = 'referee-app-cache-v0.4.57';
 const urlsToCache = [
   '/',
   '/index.html',
