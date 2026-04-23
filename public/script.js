9d3cacebdeb8a9dfdc05376cb3e8471bedd72d98
Jon Bradley <jon@ninefootone.co.uk>
2026-04-23 09:23:16 +0100

    Update home page logo, bump to v0.4.67

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const appVersion = '0.4.66';
+const appVersion = '0.4.67';
 console.log(`Referee App - Version: ${appVersion}`);
 
 /**
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'referee-app-cache-v0.4.66';
+const CACHE_NAME = 'referee-app-cache-v0.4.67';
 const urlsToCache = [
   '/',
   '/index.html',
